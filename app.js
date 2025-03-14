8716959504f26c0605766ad0d8effa73b772110e
SantiagoEL09 <santiagoestrada0990@gmail.com>
2025-03-14 17:00:51 -0500

    Creacion de funcion: Actualizar  Tarea

--- app.js
+++ app.js
@@ -23,3 +23,16 @@ function consultar(){
     document.getElementById("hora_Tarea").value = hora;
     document.getElementById("descripcion").value = descripcion;    
 }
+
+// Actualizar Tarea
+function actualizar(){
+    let nombre = document.getElementById("nombre_Tarea").value;
+    let fecha = document.getElementById("fecha_Tarea").value;
+    let hora = document.getElementById("hora_Tarea").value;
+    let descripcion = document.getElementById("descripcion").value;
+
+    localStorage.setItem("nombre_Tarea", nombre);
+    localStorage.setItem("fecha_Tarea", fecha);
+    localStorage.setItem("hora_Tarea", hora);
+    localStorage.setItem("descripcion", descripcion);
+}
